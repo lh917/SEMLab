f53765683e8b49363adf2393f91ba956742bedea
Han <liangyu.han17@imperial.ac.uk>
2020-07-29 02:01:49 +0100

    Popup version 1

--- SEM Prototype/SEMproto/View Controllers/ViewController.swift
+++ SEM Prototype/SEMproto/View Controllers/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     // Set number of models
     let numTabs:Int = 6
     
+    // Popup window controller
+    var modalDialog:ModalPopViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,6 +33,13 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         // Set view controller as datasource and delegate of the collection view
         mainMenuCollectionView.dataSource = self
         mainMenuCollectionView.delegate = self
+        
+        // Init modal dialog controller
+        if #available(iOS 13.0, *) {
+            modalDialog = storyboard?.instantiateViewController(identifier: "ModalVC") as? ModalPopViewController
+        } else {
+            // TODO: Fallback on earlier versions
+        }
 
     }
     
@@ -53,6 +63,15 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         cell.configureTab(tab: tab)
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+//        let cell = collectionView.cellForItem(at: indexPath) as? TabCollectionViewCell
+        if modalDialog != nil {
+            present(modalDialog!, animated: true, completion: nil)
+        }
+        
     }
     
 
